3e59d6dc1c7d664439866e12c40da0e9c72d8def
gzfanghaoyu <fhy1839stc@163.com>
2019-02-07 15:19:29 +0800

    f

--- logic.js
+++ logic.js
@@ -1,28 +1,33 @@
 const fs = require('fs');
 const path = require('path');
+const {ipcRenderer} = require('electron');
+const config = JSON.parse(fs.readFileSync('./pathConfig.json'));
+console.log(config.trunkPath);
 
-exports.onCopyClick = function (btnIns)
-{
-  var files=[];
-  fs.readdirSync('.').forEach(function (file) {
-      var pathname = path.join('.', file);
+exports.onCopyClick = function (btnIns) {
+    if(config.trunkPath=="" || config.stablePath=="")
+    {
+    }
+    ipcRenderer.sendSync("fhyhahahahah");
+    var files=[];
+    fs.readdirSync('.').forEach(function (file) {
+        var pathname = path.join('.', file);
 
-      if (!fs.statSync(pathname).isDirectory()) {
-          files.push(pathname);
-      }
-  });
-  console.log(files);
+        if (!fs.statSync(pathname).isDirectory()) {
+            files.push(pathname);
+        }
+    });
+    console.log(files);
 
-  var selectJson = btnIns.value;
-  if (files.find(function(e){return e==selectJson;}) ) {
-      console.log("fhy right");
-  }
-  else
-  {
-    console.log("fhy wrong");
-  }
+    var selectJson = btnIns.value;
+    if (files.find(function (e) { return e == selectJson; })) {
+        console.log("fhy right");
+    }
+    else {
+        console.log("fhy wrong");
+    }
 }
 
-exports.onInputChange = function(textIns){
+exports.onInputChange = function (textIns) {
     console.log(textIns.value);
 }
--- renderer.js
+++ renderer.js
@@ -1,3 +1,40 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const fs = require('fs');
+const path = require('path');
+const {ipcRenderer} = require('electron');
+const config = JSON.parse(fs.readFileSync('./pathConfig.json'));
+console.log(config.trunkPath);
+
+var btn=document.getElementById('copyButton');
+var textFiled = document.getElementById('jsonName');
+
+
+//搬运按钮
+btn.addEventListener('click', (event)=>{
+    if(config.trunkPath=="" || config.stablePath=="")
+    {
+        ipcRenderer.send("fhyhahahahah");
+        return;
+    }
+    var files=[];
+    fs.readdirSync('.').forEach(function (file) {
+        var pathname = path.join('.', file);
+
+        if (!fs.statSync(pathname).isDirectory()) {
+            files.push(pathname);
+        }
+    });
+    console.log(files);
+
+    var selectJson = textFiled.value;
+    if (files.find(function (e) { return e == selectJson; })) {
+        console.log("fhy right");
+    }
+    else {
+        console.log("fhy wrong");
+        return;
+    }
+})
+
+textFiled.addEventListener('change', (event)=>{
+    console.log(this.value);
+})
